cfb3283bb3e825523e7e77f9fcbe7bc546e603b4
leansellerdev <danil.d776@gmail.com>
2023-01-09 12:40:26 +0600

    get_daily_send_rates func added

--- exchange_rate.py
+++ exchange_rate.py
@@ -64,3 +64,37 @@ def get_buy_rates():
     print('Курс покупки обновлен')
 
     return EXCHANGES
+
+
+def get_daily_send_rates():
+
+    soup = BeautifulSoup(responce.content, 'html.parser')
+
+    try:
+        USD = [soup.find_all("td", {"class": "buy delta-neutral"})[0].text,
+               soup.find_all("td", {"class": "sell delta-neutral"})[0].text]
+        EUR = [soup.find_all("td", {"class": "buy delta-neutral"})[1].text,
+               soup.find_all("td", {"class": "sell delta-neutral"})[1].text]
+        RUB = [soup.find_all("td", {"class": "buy delta-neutral"})[2].text,
+               soup.find_all("td", {"class": "sell delta-neutral"})[2].text]
+        KGS = [soup.find_all("td", {"class": "buy delta-neutral"})[3].text,
+               soup.find_all("td", {"class": "sell delta-neutral"})[3].text]
+        GBP = [soup.find_all("td", {"class": "buy delta-neutral"})[4].text,
+               soup.find_all("td", {"class": "sell delta-neutral"})[4].text]
+        CNY = [soup.find_all("td", {"class": "buy delta-neutral"})[5].text,
+               soup.find_all("td", {"class": "sell delta-neutral"})[5].text]
+        GOLD = [soup.find_all("td", {"class": "buy delta-neutral"})[6].text,
+                soup.find_all("td", {"class": "sell delta-neutral"})[6].text]
+    except Exception as ex:
+        print(ex)
+        print('В данный момент невозможно обновить курс, попробуйте позже!')
+
+    EXCHANGES = {"USD": USD,
+                 "EUR": EUR,
+                 "RUB": RUB,
+                 "KGS": KGS,
+                 "GBP": GBP,
+                 "CNY": CNY,
+                 "GOLD": GOLD}
+
+    return EXCHANGES
