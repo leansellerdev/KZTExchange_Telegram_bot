69764d1b0a23afe9c874b1cb4b1eea953f1a4af0
leansellerdev <danil.d776@gmail.com>
2023-02-26 18:09:52 +0600

    config_data file added

---
+++ misc/config_data.py
@@ -0,0 +1,32 @@
+from dataclasses import dataclass
+from environs import Env
+
+MIG_URL = "https://mig.kz/"
+API_URL = "https://cdn.jsdelivr.net/gh/fawazahmed0/currency-api@1/"
+
+
+@dataclass
+class TgBot:
+    token: str
+    admin_ids: list[int]
+
+
+@dataclass
+class Api:
+    mig_api: str
+    google_api: str
+
+
+@dataclass
+class Config:
+    tg_bot: TgBot
+    apis: Api
+
+
+def load_config(path: str) -> Config:
+    env: Env = Env()
+    env.read_env(path)
+
+    return Config(tg_bot=TgBot(token=env('BOT_TOKEN'),
+                               admin_ids=list(map(int, env.list('ADMIN_IDS')))),
+                  apis=Api(MIG_URL, API_URL))
