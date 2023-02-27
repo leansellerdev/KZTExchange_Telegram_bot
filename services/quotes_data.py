211216dabb6e9b54930254a86c3cef5082001d33
ddudk <danil.d776@gmail.com>
2023-02-27 15:43:10 +0600

    collect_data, save_data, get_data funcs were added

--- services/quotes_data.py
+++ services/quotes_data.py
@@ -1,18 +1,26 @@
-from api.api import Exchange
 from datetime import datetime
 import json
+from api.api import rates
 
-rates = Exchange()
 
-
-def save_rates_to_file():
+def collect_data():
     google = rates.get_google_exchange('latest')
     mig = rates.get_mig_exchange()
     response_time = datetime.now().strftime("%d:%m:%Y %H:%M")
 
     json_file = {"update_time": response_time, "google": google, "mig": mig}
+    return json.dumps(json_file, indent=4)
 
-    json_object = json.dumps(json_file, indent=4)
+
+def save_data():
+    json_object = collect_data()
 
     with open("services/quotes.json", "w") as outfile:
         outfile.write(json_object)
+
+
+def get_data():
+    file = open("services/quotes.json")
+
+    if file:
+        return json.load(file)
