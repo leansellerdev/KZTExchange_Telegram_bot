415d30938668175cbce887fce112af313340f27b
ddudk <danil.d776@gmail.com>
2023-02-28 20:09:23 +0600

    variables renamed

--- keyboards/currencies_buttons.py
+++ keyboards/currencies_buttons.py
@@ -2,12 +2,11 @@ from aiogram.utils.keyboard import ReplyKeyboardBuilder
 from aiogram.types import KeyboardButton
 
 # Создаем объект KeyboardBulder'а
-kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+currency_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 
 # Список с валютами
-currency_list = ['USD', 'EUR', 'RUB', 'KGS', 'GBP', 'CNY', 'GOLD']
+currency_list = ['USD', 'EUR', 'RUB', 'KGS', 'GBP', 'CNY', 'GOLD', 'Назад']
 buttons: list[KeyboardButton] = [KeyboardButton(text=value) for value in currency_list]
 
 # Создаем кнопки
-kb_builder.row(*buttons, width=3)
-
+currency_kb_builder.row(*buttons, width=3)
