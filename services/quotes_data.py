65808e4fae58e40528f8dfc407ae1804f9ed2165
ddudk <danil.d776@gmail.com>
2023-02-28 20:11:32 +0600

    class Data was created

--- services/quotes_data.py
+++ services/quotes_data.py
@@ -3,24 +3,29 @@ import json
 from api.api import rates
 
 
-def collect_data():
-    google = rates.get_google_exchange('latest')
-    mig = rates.get_mig_exchange()
+class Data:
+
+    google_data = rates.get_google_exchange('latest')
+    mig_data = rates.get_mig_exchange()
     response_time = datetime.now().strftime("%d:%m:%Y %H:%M")
+    file = "services/quotes.json"
 
-    json_file = {"update_time": response_time, "google": google, "mig": mig}
-    return json.dumps(json_file, indent=4)
+    def collect_data(self):
+
+        json_file = {"update_time": self.response_time, "google": self.google_data, "mig": self.mig_data}
+        return json.dumps(json_file, indent=4)
+
+    def save_data(self):
+        json_object = self.collect_data()
+
+        with open("services/quotes.json", "w") as outfile:
+            outfile.write(json_object)
+
+    def get_data(self):
+        file = open(self.file)
+
+        if file:
+            return json.load(file)
 
 
-def save_data():
-    json_object = collect_data()
-
-    with open("services/quotes.json", "w") as outfile:
-        outfile.write(json_object)
-
-
-def get_data():
-    file = open("services/quotes.json")
-
-    if file:
-        return json.load(file)
+data = Data()
