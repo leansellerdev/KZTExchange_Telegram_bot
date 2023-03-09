3cf0387883521347ff40d61e51897d748a87ae9e
ddudk <danil.d776@gmail.com>
2023-03-09 15:28:30 +0600

    change_currency_button added

--- keyboards/action_buttons.py
+++ keyboards/action_buttons.py
@@ -8,8 +8,13 @@ from aiogram.types import KeyboardButton
 
 
 start_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+change_currency_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 
-start_buttons_list = ["Работа с валютами", "Подписаться"]
+change_currency_btn = ["🔁 Сменить валюту"]
+change_currency_keyboard: list[KeyboardButton] = [KeyboardButton(text=value) for value in change_currency_btn]
+
+start_buttons_list = ["💲Работа с валютами", "📝Подписаться"]
 start_buttons: list[KeyboardButton] = [KeyboardButton(text=value) for value in start_buttons_list]
 
 start_kb_builder.row(*start_buttons, width=2)
+change_currency_builder.row(*change_currency_keyboard)
