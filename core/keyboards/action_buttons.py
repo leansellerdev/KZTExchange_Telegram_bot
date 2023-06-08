0a280fae6f31c5743e1ae47f6321ae7aaa8ae101
ddudk <danil.d776@gmail.com>
2023-06-08 18:10:39 +0600

    contact_buttons added

--- core/keyboards/action_buttons.py
+++ core/keyboards/action_buttons.py
@@ -9,12 +9,17 @@ from aiogram.types import KeyboardButton
 
 start_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 change_currency_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
+contact_kb_builder: ReplyKeyboardBuilder = ReplyKeyboardBuilder()
 
 change_currency_btn = ["🔁 Сменить валюту"]
 change_currency_keyboard: list[KeyboardButton] = [KeyboardButton(text=value) for value in change_currency_btn]
 
-start_buttons_list = ["💲Работа с валютами", "📝Подписаться"]
+start_buttons_list = ["💲Работа с валютами", "📝 Подписаться", "☎️ Контакты"]
 start_buttons: list[KeyboardButton] = [KeyboardButton(text=value) for value in start_buttons_list]
+
+contact_buttons_list: list = ["Ауэзовский", "🔙 Назад"]
+contact_buttons: list[KeyboardButton] = [KeyboardButton(text=value) for value in contact_buttons_list]
 
 start_kb_builder.row(*start_buttons, width=2)
 change_currency_builder.row(*change_currency_keyboard)
+contact_kb_builder.row(*contact_buttons)
